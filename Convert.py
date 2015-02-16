a34ec6997deebb95d80f61d412be298329e42478
OliPicard <oliver@ufomail.co.uk>
2015-02-16 18:41:31 +0000

    Fixed GB > kB & kB > GB

--- Convert.py
+++ Convert.py
@@ -3,31 +3,34 @@ import sys
 import math
 
 #Conversions Calculator created by OliPicard after reviewing some old Python 2 code online.
-#Big thanks to the original stackoverflow article that inspired me. http://stackoverflow.com/questions/14228824/python-conversion-calculator-loop-code-efficiency
+#Refactored & updated with Digital Unit conversions
+#If you experience any bugs/problems open a ticket on github.com/olipicard/convert.py
+#Full Credits at github.com/olipicard/convert.py
 
 
 #Define the Menu
 def menu():
     print("-" * 25)
-    print("\n Digital / Conversion Calculator by \n OliPicard \n full source code @ github.com/olipicard ")
+    print("\n Digital / Conversion Calculator by \n OliPicard \n full source code @ http://github.com/olipicard ")
     print("-" * 25)
     print("1. Kilobytes to Gigabytes\n")
     print("2. MegaBytes to Gigabytes\n")
     print("3. Gigabytes to MegaBytes\n")
-    print ("4. Quit\n")
+    print("4. Gigabytes to Kilobytes\n")
+    print ("5. Quit\n")
     print ("-" * 25)
     print ("\n")
 
 #KBs to Gigabytes
 def kilobytes_to_gigabytes(a):
-    b = 1.19209289551E-7
-    c = a * b
+    b = 1048576
+    c = a / b
     return (a,float (c))
 
 #GB to KBs.
 def gigabytes_to_kilobytes(a):
-    b = 1.19209289551E-7
-    c = a / b
+    b = 1048576
+    c = a * b
     return (a, float (c))
 
 #MBs to Gigabytes
@@ -55,7 +58,7 @@ loop = True
 while loop:
     menu()
     choice = int(input("Choose your option: "))
-    if choice not in [1,2,3,4]:
+    if choice not in [1,2,3,4,5]:
         print(choice, " is not a vaild input. please try again.")
         menu()
         choice = int(input("Pick a conversion."))
@@ -67,7 +70,7 @@ while loop:
         print('{} kilobytes = gigabytes {}'.format(*result))
 
     if choice == 2:
-        print("Please enter the Megabytes you wish to convert to Gigabytes")
+        print("Please enter the Megabytes you wish to convert to Gigabytes.")
         megabytes = check_input(input("Megabytes: "))
         result = megabytes_to_gigabytes(megabytes)
         print('{} megabytes = gigabytes {}'.format(*result))
@@ -78,10 +81,16 @@ while loop:
         result = gigabytes_to_megabytes(gigabytes)
         print('{} gigabytes = megabytes {}'.format(*result))
 
-
     if choice == 4:
+        print("Please enter the Gigabytes you wish to convert to kilobytes.")
+        gigabytes = check_input(input("Gigabytes: "))
+        result = gigabytes_to_kilobytes((gigabytes))
+        print('{} gigabytes = kilobytes {}'.format(*result))
+
+    if choice == 5:
         loop = False
 
 print("\n")
 print ("-", 25)
 print ("Thanks for using Convert.py. I hope it has proven to be useful.")
+print ("If you experienced any bugs, feel free to report them at http://github.com/olipicard/Convert.py")
